12baef0c80eeddb526a771dc1ae836fbc0205cea
hegdevishwa <vishwamitra.hegde@gmail.com>
2019-06-19 00:53:57 +0100

    Initial commit

---
+++ modules/network/network.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags={
+      Name="main"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id=aws_vpc.main.id
+  cidr_block= cidrsubnet(aws_vpc.main.cidr_block,4,1)
+  tags={
+      Name="private-main"
+      VPC="main"
+      Type="private"
+  }
+  count=2
+  
+}
+
+resource "aws_subnet" "public" {
+  vpc_id=aws_vpc.main.id
+  cidr_block= cidrsubnet(aws_vpc.main.cidr_block,4,2)
+  tags={
+      Name="public-main"
+      VPC="main"
+      Type="public"
+  }
+  
+}
