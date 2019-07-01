7948ae8a1b216e20ea1ecf9d35b5bc631c66abfc
Vishwamitra Hegde <vishwamitra.hegde@gmail.com>
2019-07-01 09:11:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region     =  var.region 
+  region     = var.region 
 }
 
 module "network" {
   source = "./modules/network/"
-  
 }
 
                                                                                                               
