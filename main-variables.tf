5456b764cbeb314d672951a2a1b0d16eb78ccf4f
Vishwamitra Hegde <vishwamitra.hegde@gmail.com>
2019-07-01 09:10:49 +0100

    Update main-variables.tf

--- main-variables.tf
+++ main-variables.tf
@@ -1,8 +1,6 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {default="eu-west-1"}
-# variable "availablity_zone_names" {
-#   dafault=["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-# }
+
 
  
