45c21c7679a25bfdc948cb9f3694087233877d19
Vishwamitra Hegde <vishwamitra.hegde@gmail.com>
2019-07-29 16:31:25 +0100

    added default cidr block as variable

--- modules/network/variable.tf
+++ modules/network/variable.tf
@@ -1,5 +1,10 @@
-variable "vpc-name" {
+variable "vpc_name" {
   description = "name for the VPC to be created"
   type        = "string"
   default     = "main"
+}
+
+variable "network_address_space" {
+  description="network cidr for the network address space in the VPC"
+  default = "10.1.0.0/16"
 }
